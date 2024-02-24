78fd7bd1b4db7741cb7b92c1412481fe5e7d5765
KVNDW <kevin.wuersch@students.fhnw.ch>
2024-02-25 00:44:55 +0100

    CK: 30min add basic functions to see if script works

--- DrinkCalculator/DrinkCalc.js
+++ DrinkCalculator/DrinkCalc.js
@@ -1,1 +1,17 @@
-const beerButton = document.getElementById('numberSlider');
+const beerButton = document.getElementById('button1');
+
+const display = document.getElementById('display');
+
+
+
+
+const updateValue = () =>{
+    display.textContent = Number(display.textContent)+1;
+
+
+}
+
+
+
+
+beerButton.addEventListener("click",updateValue)

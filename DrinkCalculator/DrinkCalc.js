c64151eacc18e2f19c6418f7336df5bf0a8cbb22
KVNDW <kevin.wuersch@students.fhnw.ch>
2024-02-25 01:05:26 +0100

    CK: 30min add first functionality (only basic)

--- DrinkCalculator/DrinkCalc.js
+++ DrinkCalculator/DrinkCalc.js
@@ -1,4 +1,5 @@
-const beerButton = document.getElementById('button1');
+const beerButton1 = document.getElementById('button1');
+const beerButton4 = document.getElementById('button4');
 
 const display = document.getElementById('display');
 
@@ -9,9 +10,11 @@ const updateValue = () =>{
     display.textContent = Number(display.textContent)+1;
 
 
+
 }
 
 
 
 
-beerButton.addEventListener("click",updateValue)
+beerButton1.addEventListener("click",updateValue);
+beerButton4.addEventListener("click",updateValue)

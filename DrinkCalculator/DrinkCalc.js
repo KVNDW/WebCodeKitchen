6773268bcd5811337fe78c02227a816686b748c7
KVNDW <kevin.wuersch@students.fhnw.ch>
2024-03-11 20:20:41 +0100

    CK: try fullscreen

--- DrinkCalculator/DrinkCalc.js
+++ DrinkCalculator/DrinkCalc.js
@@ -15,13 +15,17 @@ const updateValue = () =>{
 
 const fullscreen = () =>{
 
-        document.documentElement.requestFullscreen();
-
+        if (document.documentElement.requestFullscreen) {
+            document.documentElement.requestFullscreen();
+        } else if (document.documentElement.mozRequestFullScreen) { // Firefox
+            document.documentElement.mozRequestFullScreen();
+        } else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari, Opera
+            document.documentElement.webkitRequestFullscreen();
+        } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
+            document.documentElement.msRequestFullscreen();
+        }
 
 }
-
-
-
 
 
 beerButton1.addEventListener("click",updateValue);

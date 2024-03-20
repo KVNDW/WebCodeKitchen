065c36714387f2999e598fc08a12ca0d094d3c97
KVNDW <kevin.wuersch@students.fhnw.ch>
2024-03-20 12:48:27 +0100

    CK: 1h try options

--- DrinkCalculator/DrinkCalc.js
+++ DrinkCalculator/DrinkCalc.js
@@ -1,4 +1,12 @@
-const beerButton1 = document.getElementById('button1');
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+const button3 = document.getElementById('button3');
+const button4 = document.getElementById('button4');
+const button5 = document.getElementById('button5');
+const button6 = document.getElementById('button6');
+const button7 = document.getElementById('button7');
+const button8 = document.getElementById('button8');
+
 
 const display = document.getElementById('display');
 
@@ -13,7 +21,6 @@ let totalCost=0;
 
 const updateValue = () =>{
     totalCost+=5;
-
 
     display.textContent = `${totalCost} CHF`;
 
@@ -32,7 +39,6 @@ const receiptprojecor =()=>{
     receipt.setAttribute('class','receipt-container');
     receipt.textContent = 'Beer 5 CHF';
 
-
     container.appendChild(receipt);
 
     return container;
@@ -49,10 +55,17 @@ const fullscreen = () =>{
         } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
             document.documentElement.msRequestFullscreen();
         }
-
 }
+
+//buttons event listener
+button1.addEventListener("click",updateValue);
+button2.addEventListener("click",updateValue);
+button3.addEventListener("click",updateValue);
+button4.addEventListener("click",updateValue);
+button5.addEventListener("click",updateValue);
+button6.addEventListener("click",updateValue);
+button7.addEventListener("click",updateValue);
+button8.addEventListener("click",updateValue);
+
+//fullscreen listener
 fulsscreenbutton.addEventListener("click",fullscreen );
-
-
-beerButton1.addEventListener("click",updateValue);
-
